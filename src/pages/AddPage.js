63e3ef6a212c7faf68f23ad227741df0a5101074
hael <haikaljawahir434@gmail.com>
2022-11-03 13:41:35 +0700

    first commit

---
+++ src/pages/AddPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+import InputNote from "../components/InputNote";
+import { useNavigate } from "react-router-dom";
+import { addNote } from "../utils/network-data";
+
+function AddPage() {
+  const navigate = useNavigate();
+
+  async function onAddNoteHandler(note) {
+    await addNote(note);
+    navigate("/");
+  }
+
+  return <InputNote addNote={onAddNoteHandler} />;
+}
+
+export default AddPage;
